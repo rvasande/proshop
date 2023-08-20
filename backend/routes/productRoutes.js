ef3cb5e517bd49a4437dd3db1bda09e69f9a1619
rushikesh <rushikeshvasande333@gmail.com>
2023-08-21 00:32:01 +0530

    third commit

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,6 +9,7 @@ import {
   getTopProducts,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
+import checkObjectId from "../middleware/CheckObjectId.js";
 
 const router = express.Router();
 
@@ -18,11 +19,11 @@ router.get("/top", getTopProducts);
 
 router
   .route("/:id")
-  .get(getProductById)
-  .put(protect, admin, updateProduct)
-  .delete(protect, admin, deleteProduct);
+  .get(checkObjectId, getProductById)
+  .put(protect, admin,checkObjectId, updateProduct)
+  .delete(protect, admin,checkObjectId, deleteProduct);
 
-router.route("/:id/reviews").post(protect, createProductReview);
+router.route("/:id/reviews").post(protect,checkObjectId, createProductReview);
 
 
 export default router;
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,6 +8,7 @@ import { useLogoutMutation } from "../slices/usersApiSlice";
 import { logout } from "../slices/authSlice";
 import { useNavigate } from 'react-router-dom'
 import SearchBox from "./SearchBox";
+import { resetCart } from "../slices/cartSlice";
 
 const Header = () => {
 
@@ -23,6 +24,7 @@ const Header = () => {
       try {
         await logoutApiCall().unwrap()
         dispatch(logout())
+        dispatch(resetCart())
         navigate('/login')
       } catch (error) {
         console.log(error);
